f7abae8209dbda578b8ea42db36450d857bb46f6
Alexander Lundervold <alexander.lundervold@gmail.com>
2015-09-07 11:35:58 +0200

    Update sandbox.sh
    point to fork

--- util/install/sandbox.sh
+++ util/install/sandbox.sh
@@ -46,7 +46,7 @@ fi
 ## Clone the configuration repository and run Ansible
 ##
 cd /var/tmp
-git clone https://github.com/edx/configuration
+git clone https://github.com/alu042/configuration
 cd configuration
 git checkout $CONFIG_VER
 
